352c6ac689e95aac9e0db75ffc4b0b8088b0c579
jharshe <jharshe@LIN25003665.corp.capgemini.com>
2021-01-21 17:11:45 +0530

    First Commit.

---
+++ server-discovery-demo/src/test/java/com/example/serverdiscoverydemo/ServerDiscoveryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverdiscoverydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerDiscoveryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
