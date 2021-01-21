c34773ecd762b1508d0607f7b21184b42c30972b
jharshe <jharshe@LIN25003665.corp.capgemini.com>
2021-01-21 17:09:17 +0530

    First Commit

---
+++ employee-search-service-client-dashboard/src/main/java/com/example/employeesearchserviceclientdashboard/EmployeeSearchServiceClientDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.example.employeesearchserviceclientdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EmployeeSearchServiceClientDashboardApplication {
+	public static void main(String[] args) {
+	SpringApplication.run(EmployeeSearchServiceClientDashboardApplication.class, args);
+	}
+}
---
+++ employee-search-service-client-dashboard/src/main/java/com/example/employeesearchserviceclientdashboard/entity/EmployeeInfo.java
@@ -0,0 +1,25 @@
+package com.example.employeesearchserviceclientdashboard.entity;
+
+public class EmployeeInfo {
+	private int eid;
+	private String name;
+	private String designamtion;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesignamtion() {
+		return designamtion;
+	}
+	public void setDesignamtion(String designamtion) {
+		this.designamtion = designamtion;
+	}
+}
